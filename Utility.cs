5a48ad419d97e04ace01860cc72fc83dc7a18623
RekiiServer <dtnhan.ete.dut@gmail.com>
2022-09-30 20:09:49 +0700

    up read

--- Utility.cs
+++ Utility.cs
@@ -31,7 +31,50 @@ namespace DevBlog
                 writer.Write(data.Gender);
                 writer.Write(",");
                 writer.Write(data.Address);
-                writer.WriteLine();
+                writer.Write("\n");
+            }
+        }
+        public static void ProcessString(string str)
+        {
+            List<Blogger> blg = new List<Blogger>();
+            string[] BlogList = str.Split('\n');
+            for (int i = 0; i < BlogList.Length; i++)
+            {
+                if (BlogList[i].Length > 1)
+                {
+                    var Blog = BlogList[i].Split(',');
+                    System.Console.WriteLine(BlogList[i]);
+                }
+            }
+                System.Console.WriteLine(BlogList.Length);
+        }
+        public static string ReadToTextFile(string path)
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    var data = sr.ReadToEnd();
+                    List<Blogger> Blg = new List<Blogger>();
+                    // while (data.Length > 0)
+                    // {
+                    ProcessString(data);
+                    // int ID = Convert.ToInt32()
+                    // Blg.Add()
+                    // Blg[0].Display();
+                    // }
+
+                    return data;
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                return "";
             }
         }
     }
